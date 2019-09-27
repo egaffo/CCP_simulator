0e6a4ba1d791ae94697d92043f76ef65a208dc9c
Enrico Gaffo <enrico.gaffo@gmail.com>
2019-09-27 11:45:06 +0200

    included steps to simulate ribominus reads of not annotated but expressed trx; plus, pruned annotation considering not expressed linear trx, not annotated but expressed trx and not annotated-not expressed trx of expressed circRNAs

--- scons/cmds.sh
+++ scons/cmds.sh
@@ -22,9 +22,25 @@ grep "^chr1" cirias.out | cut -f 3 | sort | uniq > circTrx.txt
 # 3. generate the transcripts FASTA(s)
 ########################
 
-grep -w "exon\|transcript" chr1.gencode.v29.annotation.gtf | grep -f circTrx.txt > cirisimTrx.gtf
-gffread cirisimTrx.gtf -w cirisimTrx.fa -g chr1.fa
-awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < cirisimTrx.fa | tail -n +2 > cirisimTrx_oneline.fa
+## (optional) select only a subset of transcripts to simulate
+## linear reads in order to mimic the cases: 
+## (i)  annotated and expressed linear trx
+## (ii) not annotated but expressed linear trx
+
+# (i) 75% of 590 = 442 transcripts/genes
+grep gene_id  ../tools/ciri_simulator/reads/cirias.out | sed -r 's/.*gene_id "([^"]+)".*/\1/' | head -442 > anno_n_xprd_genes.txt
+
+# (ii) 10% of 590 = 59 transcripts/genes
+grep gene_id  ../tools/ciri_simulator/reads/cirias.out | sed -r 's/.*gene_id "([^"]+)".*/\1/' | head -501 | tail -59 > unknown_but_xprd_genes.txt
+
+cat anno_n_xprd_genes.txt unknown_but_xprd_genes.txt > lin_trx_read_genes.txt
+
+grep -w "exon\|transcript" chr1.gencode.v29.annotation.gtf | grep -f lin_trx_read_genes.txt > lin_trx_read_genes.gtf
+
+## the following FASTA file will be not necessary when Polyester R script 
+## will consider the GTF of interest
+gffread lin_trx_read_genes.gtf -w lin_trx_read_genes.fa -g chr1.fa
+awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < lin_trx_read_genes.fa | tail -n +2 > lin_trx_read_genes_oneline.fa
 
 ########################
 # 4. simulate linear reads from the transcripts by means of polyester
@@ -45,5 +61,28 @@ Rscript R_polyester/simulate_reads.R
 cat ../ciri_simulator/cirias_1.fq.gz ../polyester/fqreads/sample_01_1.fq.gz > sim_ribo_01_1.fq.gz
 cat ../ciri_simulator/cirias_2.fq.gz ../polyester/fqreads/sample_01_2.fq.gz > sim_ribo_01_2.fq.gz
 
+
+########################
+# 7. prune original annotation to simulate unknown transcripts
+########################
+
+## select ~50% of genes that was used to generate circRNA reads by circ_simulator
+#grep gene_id  ../tools/ciri_simulator/reads/cirias.out | sed -r 's/.*gene_id "([^"]+)".*/\1/' | head -295 > genes_to_skip.txt
+## generate annotation file without the annotation of the genes selected above 
+#grep -v -f genes_to_skip.txt chr1.gencode.v29.annotation.gtf > pruned.chr1.gencode.v29.annotation.gtf
+
+## select 
+## (i)   75% of genes that was used to generate circRNA reads by circ_simulator, plus 
+## (ii)  10% of not expressed linear transcripts/genes, and exclude 
+## (iii) 10% annotation that will mimic linear transcript expressed but not annotated. Also, exclude
+## (iv)   5% annotation that will mimic expressed circRNAs with no annotation and no lin trx expressed
+
+# (ii)
+grep gene_id  ../tools/ciri_simulator/reads/cirias.out | sed -r 's/.*gene_id "([^"]+)".*/\1/' | head -560 | tail -59 > known_not_xprd_genes.txt
+
+cat anno_n_xprd_genes.txt known_not_xprd_genes.txt > genes_to_keep.txt
+
+## generate annotation file without the annotation of the genes selected above 
+grep -f genes_to_keep.txt chr1.gencode.v29.annotation.gtf > pruned.chr1.gencode.v29.annotation.gtf
 
 
