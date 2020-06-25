5e94ec4f3fc29288ff16e223029ab81713099d88
Enrico Gaffo <enrico.gaffo@gmail.com>
2020-06-25 16:41:13 +0200

    update install script to invoke for installation

--- utils/install.sh
+++ utils/install.sh
@@ -1,9 +1,26 @@
-## ciri simulator
-wget https://sourceforge.net/projects/ciri/files/CIRI_simulator.pl -O bin/CIRI_simulator.pl
+#!/bin/bash
 
-# ## gffread from Cufflinks
-# wget http://cole-trapnell-lab.github.io/cufflinks/assets/downloads/cufflinks-2.2.1.Linux_x86_64.tar.gz -O bin/cufflinks-2.2.1.Linux_x86_64.tar.gz
-# tar xf bin/cufflinks-2.2.1.Linux_x86_64.tar.gz
-# ln -s cufflinks-2.2.1.Linux_x86_64/gffread
+export CCPSIM_HOME=$(dirname $(readlink -f $0))/../
 
-## Polyester
+mkdir -p $CCPSIM_HOME/bin
+
+export PATH=$CCPSIM_HOME/bin/:$PATH
+## export environment variable for local R repository
+export R_LIBS=$CCPSIM_HOME/tools/R_libs
+
+## install Scons
+cd $CCPSIM_HOME/bin;
+## -N check if newer version is online; -n just check whether the file exists and does not send HTTP requests
+wget  -nc http://prdownloads.sourceforge.net/scons/scons-local-3.1.2.tar.gz;
+mkdir -p scons_dir
+tar -xf scons-local-3.1.2.tar.gz -C scons_dir ;
+ln -sf $CCPSIM_HOME/bin/scons_dir/scons.py $CCPSIM_HOME/bin/scons;
+cd -
+
+cd $CCPSIM_HOME/bin
+ln -sf ../utils/*.py .
+ln -sf ../utils/*.R .
+cd -
+
+## install other tools
+$CCPSIM_HOME/bin/scons -i -f $CCPSIM_HOME/scons/install.py $1
