f744bc07d84d2f05cf0628a88e4ba4da98facb99
Enrico Gaffo <enrico.gaffo@gmail.com>
2020-06-25 09:28:50 +0200

    removed required fasta sequences for transcripts: now just GTF and reference genome are required

--- utils/simulate_linear_trx_reads.R
+++ utils/simulate_linear_trx_reads.R
@@ -9,14 +9,18 @@ suppressPackageStartupMessages(library(polyester))
 suppressPackageStartupMessages(library(Biostrings))
 
 option_list <- list(
-    make_option(c("-f", "--trx_fasta"), action = "store", type = "character",
-                help = "transcripts.fa"),
+    make_option(c("-f", "--fasta"), action = "store", type = "character",
+                help = "Transcript or reference sequences in FASTA format"),
     make_option(c("-o", "--outdir"), action = "store", type = "character",
                 default = "./",
                 help = "Output directory were sample_xx_x.fasta will be saved"),
     make_option(c("-c", "--coverage"), action = "store", type = "numeric",
                 default = 20,
-                help = "Transcript coverage. Default 20 for ~20x coverage")
+                help = "Transcript coverage. Default 20 for ~20x coverage"),
+    make_option(c("-g", "--gtf"), action = "store", type = "character",
+                default = NULL,
+                help = paste0("Gene annotation in GTF format. If this is set, ",
+                              "then the --fasta must refer to reference sequences"))
 )
 
 parser <- OptionParser(usage = "%prog -f transcripts.fa -o lin_simulated_reads",
@@ -27,21 +31,42 @@ arguments <- parse_args(parser, positional_arguments=F)
 results.path <- arguments$outdir
 dir.create(path = results.path, recursive = T, showWarnings = F)
 
-fasta = readDNAStringSet(arguments$trx_fasta)
-
+# fasta = readDNAStringSet(arguments$fasta)
+#
+# fold_changes = matrix(1,
+#                       ncol = 2,
+#                       nrow = length(fasta))
+n.trx <- sum("transcript" == scan(file = arguments$gtf,
+                                  what = "character"),
+             na.rm = T)
 fold_changes = matrix(1,
-                      ncol = 2,
-                      nrow = length(fasta))
-
-# ~20x coverage ----> reads per transcript = transcriptlength/readlength * 20
-# here all transcripts will have ~equal FPKM
-readspertx = round(as.numeric(arguments$coverage) * width(fasta) / 100)
+                      ncol = 1,
+                      nrow = n.trx)
+#
+# # ~20x coverage ----> reads per transcript = transcriptlength/readlength * 20
+# # here all transcripts will have ~equal FPKM
+# readspertx = round(as.numeric(arguments$coverage) * width(fasta) / 100)
 
 # simulation call:
-simulate_experiment(arguments$trx_fasta,
-                    reads_per_transcript = readspertx,
-                    num_reps = c(1, 1),
-                    fold_changes = fold_changes,
-                    outdir = results.path,
-                    strand_specific = F,
-                    paired = T)
+if(!is.null(arguments$gtf)){
+    simulate_experiment(seqpath = arguments$fasta,
+                        gtf = arguments$gtf,
+                        # reads_per_transcript = readspertx,
+                        reads_per_transcript = 300,
+                        num_reps = c(1, 1), #c(1),
+                        fold_changes = fold_changes,
+                        # meanmodel = TRUE,
+                        outdir = results.path,
+                        strand_specific = F,
+                        paired = T)
+}else{
+    simulate_experiment(fasta = arguments$fasta,
+                        # reads_per_transcript = readspertx,
+                        reads_per_transcript = 300,
+                        num_reps = c(1), #c(1, 1),
+                        fold_changes = fold_changes,
+                        # meanmodel = TRUE,
+                        outdir = results.path,
+                        strand_specific = F,
+                        paired = T)
+}
