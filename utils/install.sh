c5d4edc897568b5de2fc00ad46f752bbfb2012a1
Enrico Gaffo <enrico.gaffo@gmail.com>
2020-06-25 15:34:16 +0200

    moved install script into utils dir

---
+++ utils/install.sh
@@ -0,0 +1,9 @@
+## ciri simulator
+wget https://sourceforge.net/projects/ciri/files/CIRI_simulator.pl -O bin/CIRI_simulator.pl
+
+# ## gffread from Cufflinks
+# wget http://cole-trapnell-lab.github.io/cufflinks/assets/downloads/cufflinks-2.2.1.Linux_x86_64.tar.gz -O bin/cufflinks-2.2.1.Linux_x86_64.tar.gz
+# tar xf bin/cufflinks-2.2.1.Linux_x86_64.tar.gz
+# ln -s cufflinks-2.2.1.Linux_x86_64/gffread
+
+## Polyester
